0d337f3de7e1f5c8a598f91c9f66b5058ef32ffd
Derzam <86030316+Derzam@users.noreply.github.com>
2021-06-24 19:24:08 -0500

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,16 @@
+package t01_programacionoo;
+
+public class Persona {
+   
+	private String nombre;
+	
+	void sacaralperro() {
+		
+		Perro perro = new Perro();
+		perro.correr(nombre);
+		
+	}
+		
+		void correr() {
+	}
+}
