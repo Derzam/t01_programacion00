57cb12ab0399e99de2e6f1863e41b65f51ce6b70
Derzam <86030316+Derzam@users.noreply.github.com>
2021-07-14 19:24:18 -0500

    Add files via upload

---
+++ mainfigurainvertida.java
@@ -0,0 +1,33 @@
+package tprogramacion_001;
+
+import java.util.Scanner;
+
+public class mainfigurainvertida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ingresa la cantidad de lineas para el triangulo");
+		int i;
+		int j;
+		
+		Scanner ob = new Scanner(System.in);
+		 int lineas ;
+		lineas = ob.nextInt();
+		
+		
+		
+		for(i =lineas; i >= 1; i--) {
+			
+			for (j = 0; j < i; j++) {
+		
+			System.out.print("*");
+			}
+			
+			System.out.println("");
+		}
+	}
+}
+	
+	 
+	
